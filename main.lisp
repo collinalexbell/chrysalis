c1ad1982c8c2c73bd22f88d608d09b5d0c123b21
kuberlog <collinalexbell@gmail.com>
2021-04-16 02:24:42 -0500

    start-gui and main

--- main.lisp
+++ main.lisp
@@ -1,3 +1,11 @@
 
-(defparameter v "0.0.1")
-(format t "Chrysalis v~$" v)
+(defpackage :chrysalis (:use :cl))
+
+(defun start-gui ()
+  (format t "Starting gui..."))
+
+(defun main ()
+  (let ((v "0.0.1"))
+   (format t "Chrysalis v~$~%" v))
+  (start-gui))
+
