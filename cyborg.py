852d166a76c8653a6b171a49d2d54a0ff2c7712f
kuberlog <collinalexbell@gmail.com>
2021-04-23 22:33:38 -0500

    extracted Cyborg into its own file

---
+++ cyborg.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+# Author: kuberlog
+# Date: 2021-04-21
+
+# Derived from example by:
+#    Kwasi Mensah (kmensah@andrew.cmu.edu)
+#    copyright 2005-08-02
+#    forked by Collin Bell on 2021-04-21
+
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
+from direct.gui.DirectGui import *
+from direct.interval.IntervalGlobal import *
+from panda3d.core import lookAt
+from panda3d.core import GeomVertexFormat, GeomVertexData
+from panda3d.core import Geom, GeomTriangles, GeomVertexWriter
+from panda3d.core import Texture, GeomNode
+from panda3d.core import PerspectiveLens
+from panda3d.core import CardMaker
+from panda3d.core import Light, Spotlight
+from panda3d.core import TextNode
+from panda3d.core import LVector3
+import sys
+import os
+import voxel
+from voxel import Voxel
+import time
+
+class Memory:
+    def __init__(self, memory_description):
+        self.description = memory_description
+        self.when = time.time()
+
+    def __repr__(self):
+        return f"{self.description} at {self.when}"
+
+class Cyborg(DirectObject):
+
+    def __init__(self):
+        self.y = -100
+        self.x = 0
+        self.z = 0
+        self.is_memoizing = False
+        self.camera_rotation = 0
+        self.accept("w", self.up)
+        self.accept("s", self.down)
+        self.accept("a", self.strafe_left)
+        self.accept("d", self.strafe_right)
+        self.accept("q", self.left)
+        self.accept("e", self.right)
+        self.accept("v", self.chrysalate)
+        self.accept("m", self.start_memoizations)
+        self.accept("n", self.stop_memoizations)
+        self.accept("p", self.play_memoization)
+        self.memory = []
+
+        self.LightsOn = False
+        self.LightsOn1 = False
+        slight = Spotlight('slight')
+        slight.setColor((1, 1, 1, 1))
+        lens = PerspectiveLens()
+        slight.setLens(lens)
+        self.slnp = render.attachNewNode(slight)
+        self.slnp1 = render.attachNewNode(slight)
+
+    def memoize_movement(self, movement):
+        if(self.is_memoizing):
+            self.memory.append(Memory(movement))
+            print(self.memory)
+
+    def start_memoizations(self):
+        self.is_memoizing = True 
+        self.memory = []
+
+    def stop_memoizations(self):
+        self.is_memoizing = False
+
+    def play_memoization(self):
+        for index, cur_memory in enumerate(self.memory):
+            if index < (len(self.memory) - 1):
+                next_memory = self.memory[index+1]
+            else:
+                last_memory = None
+            if(cur_memory.description == "left"):
+                print("left")
+                self.left()
+            elif (cur_memory.description == "right"):
+                print("right")
+                self.right()
+            elif (cur_memory.description == "strafe_left"):
+                print("strafe_left")
+                self.strafe_left()
+            elif (cur_memory.description == "strafe_right"):
+                print("strafe_right")
+                self.strafe_right()
+            elif (cur_memory.description == "up"):
+                print("up")
+                self.up()
+            elif (cur_memory.description == "down"):
+                print("down")
+                self.down()
+            
+            if next_memory is not None and (next_memory.when - cur_memory.when) > 0:
+                sleep_time = next_memory.when - cur_memory.when
+                print(sleep_time)
+                time.sleep(sleep_time)
+
+    def left(self):
+        self.camera_rotation = self.camera_rotation + 10
+        base.camera.setHpr(self.camera_rotation, 0, 0)
+        self.memoize_movement("left")
+
+    def right(self):
+        self.camera_rotation = self.camera_rotation - 10
+        base.camera.setHpr(self.camera_rotation, 0, 0)
+        self.memoize_movement("right")
+
+    def up(self):
+        self.y = self.y + 10
+        base.camera.setPos(self.x, self.y, self.z)
+        self.memoize_movement("up")
+        
+    def down(self):
+        self.y = self.y - 10
+        base.camera.setPos(self.x, self.y, self.z)
+        self.memoize_movement("down")
+        
+    def strafe_left(self):
+        self.x = self.x - 10
+        base.camera.setPos(self.x, self.y, self.z)
+        self.memoize_movement("strafe_left")
+
+    def strafe_right(self):
+        self.x = self.x + 10
+        base.camera.setPos(self.x, self.y, self.z)
+        self.memoize_movement("strafe_right")
+
+    def chrysalate(self):
+        brick = Voxel(self.x, self.y, self.z)
+        brick = render.attachNewNode(brick.get_panda_render_node())
+        # OpenGl by default only draws "front faces" (polygons whose vertices are specified CCW).
+        # I may want to get rid of this if not needed
+        brick.setTwoSided(True)
+
